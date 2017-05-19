dc4a84655cde7ab0555766857e04e18912c1bbc5
heymiguel <miguelcesarbautista@gmail.com>
2017-05-18 21:34:38 -0400

    Lesson 3

--- src/app.js
+++ src/app.js
@@ -1,36 +1,63 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import NoteCard from './NotesCard.js'
 
 class App extends React.Component {
+  constructor() {
+    super();
+    this.toggleSidebar = this.toggleSidebar.bind( this );
+    this.addNote = this.addNote.bind( this );
+
+    // state goes here
+    this.state = {
+      notes: []
+    }
+  }
+
+  toggleSidebar( e ) {
+    e.preventDefault();
+    this.sidebar.classList.toggle( "show" );
+  }
+
+  addNote( e ) {
+    e.preventDefault();
+    const note = {
+      title: this.noteTitle.value,
+      text: this.noteText.value
+    };
+    const newNotes = Array.from( this.state.notes );
+    newNotes.push( note );
+    this.setState( {
+      notes: newNotes
+    } );
+  }
+
   render() {
     return (
       <div>
         <header className="mainHeader">
           <h1>Noted</h1>
           <nav>
-            <a href="">Add Note</a>
+            <a href="" onClick={this.toggleSidebar}>Add Note</a>
           </nav>
         </header>
         <section className="notes">
-          <div className="noteCard">
-            <i className="fa fa-edit"></i>
-            <i className="fa fa-times"></i>
-            <h4>Test Note</h4>
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perspiciatis nihil culpa recusandae asperiores at eveniet provident, hic sit. Mollitia nam accusantium aliquam officiis, est, at consectetur pariatur qui quaerat officia.
-            </p>
-          </div>
+          {this.state.notes.map(( note, i ) => {
+            return (
+              <NoteCard note={note} key={`note-${ i }`} />
+            )
+          } )}
         </section>
-        <aside className="sidebar">
-          <form action="">
+        <aside className="sidebar" ref={ref => this.sidebar = ref}>
+          <form action="" onSubmit={this.addNote}>
             <h3>Add new note</h3>
-            <div className="close-btn">
+            <div className="close-btn" onClick={this.toggleSidebar}>
               <i className="fa fa-times"></i>
             </div>
             <label htmlFor="note-title">Title:</label>
-            <input type="text" name="note-title" />
-            <label htmlFor="note-text"></label>
-            <textarea name="note-text" id="" cols="30" rows="10"></textarea>
+            <input type="text" name="note-title" ref={ref => this.noteTitle = ref} />
+            <label htmlFor="note-text"> Text: </label>
+            <textarea name="note-text" id="" ref={ref => this.noteText = ref}></textarea>
             <input type="submit" value="add new note" />
           </form>
         </aside>
