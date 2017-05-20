986e4aaa6ed19e7ab728d10d99a8b1a395a38d39
heymiguel <miguelcesarbautista@gmail.com>
2017-05-19 20:33:01 -0400

    broken.

--- src/NotesCard.js
+++ src/NotesCard.js
@@ -1,14 +1,58 @@
 import React, { Component } from 'react';
 
-export default function ( props ) {
-  return (
-    <div className="noteCard">
-      <i className="fa fa-edit"></i>
-      <i className="fa fa-times"></i>
-      <h4>{props.note.title}</h4>
-      <p>
-        {props.note.text}
-      </p>
-    </div>
-  )
+class NoteCard extends Component {
+  constructor() {
+    super();
+    this.state = {
+      editing: false,
+      note: {}
+    }
+    this.saveNote = this.saveNote.bind( this );
+  }
+
+  componentDidMount() {
+
+    this.setState( {
+      note: this.props.note
+    } );
+    console.log( this.state );
+  }
+
+  saveNote( e ) {
+    e.preventDefault();
+
+  }
+
+  render() {
+    let isEditing = (
+      <div>
+        <h4>{this.state.note.title}</h4>
+        <p>
+          {this.state.note.text}
+        </p>
+      </div>
+    )
+    if ( this.state.editing ) {
+      isEditing = (
+        <form onSubmit={this.saveNote}>
+          <div>
+            <input type="text" defaultValue={this.state.note.title} name='title' ref={ref = this.noteTitle = ref} />
+          </div>
+          <div>
+            <input type="text" defaultValue={this.state.note.text} name='text' ref={ref = this.noteText = ref} />
+          </div>
+          <input type="submit" value="Done editing!" />
+        </form>
+      )
+    }
+    return (
+      <div className="noteCard">
+        <i className="fa fa-edit" onClick={() => this.setState( { editing: true } )}></i>
+        <i className="fa fa-times" onClick={() => this.props.removeNote( this.state.note.key )}></i>
+        {isEditing}
+      </div>
+    );
+  }
 }
+
+export default NoteCard;
--- src/app.js
+++ src/app.js
@@ -2,17 +2,43 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import NoteCard from './NotesCard.js'
 
+var config = {
+  apiKey: "AIzaSyD6LJ-lVcpA0g9WYz0YC1ojH1rwgd3hAYU",
+  authDomain: "snsv2-560be.firebaseapp.com",
+  databaseURL: "https://snsv2-560be.firebaseio.com",
+  projectId: "snsv2-560be",
+  storageBucket: "snsv2-560be.appspot.com",
+  messagingSenderId: "1012416512995"
+};
+firebase.initializeApp( config );
+
 class App extends React.Component {
   constructor() {
     super();
     this.toggleSidebar = this.toggleSidebar.bind( this );
     this.addNote = this.addNote.bind( this );
 
-    // state goes here
+
     this.state = {
       notes: []
     }
   }
+
+  componentDidMount() {
+    firebase.database().ref().on( 'value', ( res ) => {
+      const userData = res.val();
+      const dataArray = [];
+      Object.keys( userData ).map(( objectKey ) => {
+        userData[ objectKey ].key = objectKey;
+        dataArray.push( userData[ objectKey ] );
+      } );
+      this.setState( {
+        notes: dataArray
+      } );
+    } );
+    // firebase.database.ref() this references the entire database
+  }
+
 
   toggleSidebar( e ) {
     e.preventDefault();
@@ -25,11 +51,18 @@ class App extends React.Component {
       title: this.noteTitle.value,
       text: this.noteText.value
     };
-    const newNotes = Array.from( this.state.notes );
-    newNotes.push( note );
-    this.setState( {
-      notes: newNotes
-    } );
+
+    const dbRef = firebase.database().ref();
+    dbRef.push( note ); // adds the note.
+
+    this.noteTitle.value = "";
+    this.noteText.value = "";
+    this.toggleSidebar( e );
+  }
+
+  removeNote( noteId ) {
+    const dbRef = firebase.database().ref( noteId ); // key here tells firebase which key
+    dbRef.remove();
   }
 
   render() {
@@ -43,10 +76,11 @@ class App extends React.Component {
         </header>
         <section className="notes">
           {this.state.notes.map(( note, i ) => {
+            console.log( note );
             return (
-              <NoteCard note={note} key={`note-${ i }`} />
+              <NoteCard note={note} key={`note-${ i }`} removeNote={this.removeNote} />
             )
-          } )}
+          } ).reverse()}
         </section>
         <aside className="sidebar" ref={ref => this.sidebar = ref}>
           <form action="" onSubmit={this.addNote}>
